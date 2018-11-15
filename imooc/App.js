895c9b436cc48016396639027be95b80722f47d6
user-dyq <dong_ya_qiang@163.com>
2018-11-15 10:59:43 +0800

    react项目

---
+++ imooc/App.js
@@ -0,0 +1,70 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow
+ */
+
+import React, {Component} from 'react';
+import {Platform, Image, StyleSheet, Text, View} from 'react-native';
+import TabNavigator  from 'react-native-tab-navigator'
+const instructions = Platform.select({
+  ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
+  android:
+    'Double tap R on your keyboard to reload,\n' +
+    'Shake or press menu button for dev menu',
+});
+
+type Props = {};
+export default class App extends Component<Props> {
+  constructor(props){
+    super(props);
+    this.state={
+      selectedTab:'home'
+    }
+  }
+  render() {
+    return (
+      <View style={styles.container}>
+         
+       <TabNavigator>
+          <TabNavigator.Item
+              // selected={this.state.selectedTab === 'home'}
+              title="Home"
+              badgeText="1"
+              onPress={() => this.setState({ selectedTab: 'home' })}>
+            <View style={styles.page1}></View>>
+          </TabNavigator.Item>
+          <TabNavigator.Item
+              // selected={this.state.selectedTab === 'profile'}
+              title="Profile"
+              badgeText="2"
+              onPress={() => this.setState({ selectedTab: 'profile' })}>
+            <View style={styles.page2}></View>>
+          </TabNavigator.Item>
+        </TabNavigator>
+        
+      </View>
+    );
+  }
+}
+
+
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  page1:{
+    flex:1,
+    backgroundColor:'red',
+  },
+  page2:{
+    flex:1,
+    backgroundColor:'yellow',
+  }
+});
